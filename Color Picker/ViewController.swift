a77b06f0bc978e255cf0178177a7b269eb21e117
Du <duulatb@gmail.com>
2021-05-13 22:50:29 +0600

    2 Start coding

--- Color Picker/ViewController.swift
+++ Color Picker/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var colorDisplay: UIView!
+    @IBOutlet var redField: UITextField!
+    @IBOutlet var greenField: UITextField!
+    @IBOutlet var blueField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorDisplay.layer.cornerRadius = 10
+        colorDisplay.backgroundColor = .systemBlue
+        
     }
 
+    
 
 }
 
